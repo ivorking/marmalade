7afc073ab554c732f5e25ea10df198e42634d347
Ivor King <ivorking@gmail.com>
2020-04-26 19:49:13 +0100

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,60 @@
 import React, {Component} from 'react';
 import Header from './Header';
 import FeaturedMix from './FeaturedMix';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => (
+  <h1>Home</h1>
+)
+
+const Archive = () => (
+  <h1>Archive</h1>
+)
+
+const About = () => (
+  <h1>About</h1>
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="flex-l justify-end">
-          { /* FeaturedMix */}
-          <FeaturedMix />
-          <div className='w-50-l relative z-1'>
-            <Header />
+      <Router>
+        <div>
+          <div className="flex-l justify-end">
+            { /* FeaturedMix */}
+            <FeaturedMix />
+            <div className='w-50-l relative z-1'>
+              <Header />
+
+              {/* React routes */}
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/archive">
+                  <Archive />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+              </Switch>
+
+            </div>
           </div>
-        </div>
-        <iframe 
-          width="100%" 
-          height="60" 
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fdarkmodulator%2Fblutengel-megamix-from-dj-dark-modulator%2F" 
-          frameBorder="0"
-          className='player db fixed bottom-0 z-5'
-        />
-      </div>   
+          <iframe 
+            width="100%" 
+            height="60" 
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fdarkmodulator%2Fblutengel-megamix-from-dj-dark-modulator%2F" 
+            frameBorder="0"
+            className='player db fixed bottom-0 z-5'
+          />
+        </div>  
+      </Router> 
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,10 @@
 import React, {Component} from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+} from "react-router-dom";
 
 const Header = props => (
     <header className='black mb5 pt5'>
@@ -6,16 +12,17 @@ const Header = props => (
         <h1 className= 'ttu f3 tracked-mega anton tc mt0 mb3'>Marmalade</h1>
         <ul className= 'list flex justify-center pl0'>
 
+            {/* First line need exact */}
             <li className='mh2'>
-                <a className = 'nav-link link black biryani-black f6 ttu gray'>what's hot</a>
+                <NavLink exact to='/' className = 'nav-link link black biryani-black f6 ttu gray'>what's hot</NavLink>
             </li>
 
             <li className='mh2'>
-                <a className = 'nav-link link black biryani-black f6 ttu gray'>archive</a>
+                <NavLink to='/archive' className = 'nav-link link black biryani-black f6 ttu gray'>archive</NavLink>
             </li>
 
             <li className='mh2'>
-                <a className = 'nav-link link black biryani-black f6 ttu gray'>about</a>
+                <NavLink to='/about' className = 'nav-link link black biryani-black f6 ttu gray'>about</NavLink>
             </li>
 
         </ul>
