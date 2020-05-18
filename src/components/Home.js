940d0c54afe61d40b2b19a9ce39d40a4bba9c08d
Ivor King <ivorking@gmail.com>
2020-05-18 23:15:35 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Mix from './Mix';
 
 const Home = () => (
     <div classNmae = 'flex flex-wrap justify-between mixes ph3 ph4-1'>
     {/* looping through mixes */}
         <div className="mix mb4">
             {/* mix goes here */}
+            <Mix />
         </div>
     </div>
 );
