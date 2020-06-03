6496736d3c7303cba317d75954db9cf5bff6373f
Ivor King <ivorking@gmail.com>
2020-06-03 08:03:50 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Mix from './Mix';
 
-const Home = (mixes, ...props) => (
+const Home = ({mixes, ...props}) => (
     <div className = 'flex flex-wrap justify-between mixes ph3 ph4-1'>
         {mixes.map(mix => (
             <div className = "mix mb4">
