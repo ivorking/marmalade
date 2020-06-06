b74c80857e99ed3e159595b550402494623c8c19
Ivor King <ivorking@gmail.com>
2020-06-06 11:24:10 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = ({mixes, ...props}) => (
     <div className = 'flex flex-wrap justify-between mixes ph3 ph4-1'>
         {mixes.map(mix => (
             <div className = "mix mb4">
-                <Mix {...props} {...mix}/>
+                <Mix {...props} {...mix} id={mix.key}/>
             </div>
         ))}
     </div>
