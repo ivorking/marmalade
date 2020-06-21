0e8ec8d5e689b9710a378b50d5b0042141c0add3
Ivor King <ivorking@gmail.com>
2020-06-21 18:14:54 +0100

    update

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Archive = props => (
+const Archive = props => <ul className = "list p10 archive">
+
     <div>
         <h1>Text</h1>
         Archive why no show
